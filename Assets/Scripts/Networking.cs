323cc4045c46eb475ad0de79ffca5b70dee2a605
Cursed Entertainment <156717952+Farica-Kimora@users.noreply.github.com>
2024-02-14 21:07:13 +0200

    ...

--- Assets/Scripts/Networking.cs
+++ Assets/Scripts/Networking.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Networking : MonoBehaviour
 {
-    private string url = "http://127.0.0.1:5000/calculate";
+    string url = "http://127.0.0.1:5000/calculate";
 
     void Start()
     {
